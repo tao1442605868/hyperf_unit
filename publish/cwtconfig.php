8bac06f8282142325bcc1e830e832855fd5e4a4c
tao1442605868 <tao1442605868@163.com>
2020-10-12 16:36:03 +0800

    iii

--- publish/cwtconfig.php
+++ publish/cwtconfig.php
@@ -14,118 +14,65 @@ return [
      * 默认配置，将会合并到各模块中
      */
     'defaults' => [
-        /*
-         * 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
-         */
-        'response_type' => 'array',
 
-        /*
-         * 使用 Laravel 的缓存系统
-         */
-        'use_laravel_cache' => true,
+    ],
+    'alipay' => [
+        // 支付宝分配的 APPID
+        'app_id' => env('ALI_APP_ID', ''),
 
-        /*
-         * 日志配置
-         *
-         * level: 日志级别，可选为：
-         *                 debug/info/notice/warning/error/critical/alert/emergency
-         * file：日志文件位置(绝对路径!!!)，要求可写权限
-         */
+        // 支付宝异步通知地址
+        'notify_url' => '',
+
+        // 支付成功后同步通知地址
+        'return_url' => '',
+
+        // 阿里公共密钥，验证签名时使用
+        'ali_public_key' => env('ALI_PUBLIC_KEY', ''),
+
+        // 自己的私钥，签名时使用
+        'private_key' => env('ALI_PRIVATE_KEY', ''),
+
+        // 使用公钥证书模式，请配置下面两个参数，同时修改 ali_public_key 为以 .crt 结尾的支付宝公钥证书路径，如（./cert/alipayCertPublicKey_RSA2.crt）
+        // 应用公钥证书路径
+        // 'app_cert_public_key' => './cert/appCertPublicKey.crt',
+
+        // 支付宝根证书路径
+        // 'alipay_root_cert' => './cert/alipayRootCert.crt',
+
+        // optional，默认 warning；日志路径为：sys_get_temp_dir().'/logs/yansongda.pay.log'
         'log' => [
-            'level' => env('WECHAT_LOG_LEVEL', 'debug'),
-            'file' => env('WECHAT_LOG_FILE', storage_path('logs/wechat.log')),
+            'file' => BASE_PATH . '/runtime/logs/alipay.log',
         ],
     ],
 
-    /*
-     * 路由配置
-     */
-    'route' => [
-        /*
-         * 开放平台第三方平台路由配置
-         */
-        // 'open_platform' => [
-        //     'uri' => 'serve',
-        //     'action' => Overtrue\LaravelWeChat\Controllers\OpenPlatformController::class,
-        //     'attributes' => [
-        //         'prefix' => 'open-platform',
-        //         'middleware' => null,
-        //     ],
-        // ],
-    ],
+    'wechat' => [
+        // 公众号 APPID
+        'app_id' => env('WECHAT_APP_ID', ''),
 
-    /*
-     * 公众号
-     */
-    'official_account' => [
-        'default' => [
-            'app_id' => env('WECHAT_OFFICIAL_ACCOUNT_APPID', 'your-app-id'),         // AppID
-            'secret' => env('WECHAT_OFFICIAL_ACCOUNT_SECRET', 'your-app-secret'),    // AppSecret
-            'token' => env('WECHAT_OFFICIAL_ACCOUNT_TOKEN', 'your-token'),           // Token
-            'aes_key' => env('WECHAT_OFFICIAL_ACCOUNT_AES_KEY', ''),                 // EncodingAESKey
+        // 小程序 APPID
+        'miniapp_id' => env('WECHAT_MINIAPP_ID', ''),
 
-            /*
-             * OAuth 配置
-             *
-             * scopes：公众平台（snsapi_userinfo / snsapi_base），开放平台：snsapi_login
-             * callback：OAuth授权完成后的回调页地址(如果使用中间件，则随便填写。。。)
-             */
-            // 'oauth' => [
-            //     'scopes'   => array_map('trim', explode(',', env('WECHAT_OFFICIAL_ACCOUNT_OAUTH_SCOPES', 'snsapi_userinfo'))),
-            //     'callback' => env('WECHAT_OFFICIAL_ACCOUNT_OAUTH_CALLBACK', '/examples/oauth_callback.php'),
-            // ],
+        // APP 引用的 appid
+        'appid' => env('WECHAT_APPID', ''),
+
+        // 微信支付分配的微信商户号
+        'mch_id' => env('WECHAT_MCH_ID', ''),
+
+        // 微信支付异步通知地址
+        'notify_url' => '',
+
+        // 微信支付签名秘钥
+        'key' => env('WECHAT_KEY', ''),
+
+        // 客户端证书路径，退款、红包等需要用到。请填写绝对路径，linux 请确保权限问题。pem 格式。
+        'cert_client' => './apiclient_cert.pem',
+
+        // 客户端秘钥路径，退款、红包等需要用到。请填写绝对路径，linux 请确保权限问题。pem 格式。
+        'cert_key' => './apiclient_key.pem',
+
+        // optional，默认 warning；日志路径为：sys_get_temp_dir().'/logs/yansongda.pay.log'
+        'log' => [
+            'file' => BASE_PATH . '/runtime/logs/wechat.log',
         ],
     ],
-
-    /*
-     * 开放平台第三方平台
-     */
-    // 'open_platform' => [
-    //     'default' => [
-    //         'app_id'  => env('WECHAT_OPEN_PLATFORM_APPID', ''),
-    //         'secret'  => env('WECHAT_OPEN_PLATFORM_SECRET', ''),
-    //         'token'   => env('WECHAT_OPEN_PLATFORM_TOKEN', ''),
-    //         'aes_key' => env('WECHAT_OPEN_PLATFORM_AES_KEY', ''),
-    //     ],
-    // ],
-
-    /*
-     * 小程序
-     */
-    // 'mini_program' => [
-    //     'default' => [
-    //         'app_id'  => env('WECHAT_MINI_PROGRAM_APPID', ''),
-    //         'secret'  => env('WECHAT_MINI_PROGRAM_SECRET', ''),
-    //         'token'   => env('WECHAT_MINI_PROGRAM_TOKEN', ''),
-    //         'aes_key' => env('WECHAT_MINI_PROGRAM_AES_KEY', ''),
-    //     ],
-    // ],
-
-    /*
-     * 微信支付
-     */
-    // 'payment' => [
-    //     'default' => [
-    //         'sandbox'            => env('WECHAT_PAYMENT_SANDBOX', false),
-    //         'app_id'             => env('WECHAT_PAYMENT_APPID', ''),
-    //         'mch_id'             => env('WECHAT_PAYMENT_MCH_ID', 'your-mch-id'),
-    //         'key'                => env('WECHAT_PAYMENT_KEY', 'key-for-signature'),
-    //         'cert_path'          => env('WECHAT_PAYMENT_CERT_PATH', 'path/to/cert/apiclient_cert.pem'),    // XXX: 绝对路径！！！！
-    //         'key_path'           => env('WECHAT_PAYMENT_KEY_PATH', 'path/to/cert/apiclient_key.pem'),      // XXX: 绝对路径！！！！
-    //         'notify_url'         => 'http://example.com/payments/wechat-notify',                           // 默认支付结果通知地址
-    //     ],
-    //     // ...
-    // ],
-
-    /*
-     * 企业微信
-     */
-    // 'work' => [
-    //     'default' => [
-    //         'corp_id' => 'xxxxxxxxxxxxxxxxx',
-    //         'agent_id' => 100020,
-    //         'secret'   => env('WECHAT_WORK_AGENT_CONTACTS_SECRET', ''),
-    //          //...
-    //      ],
-    // ],
 ];
